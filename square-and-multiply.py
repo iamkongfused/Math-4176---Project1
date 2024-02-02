fd928c4e2d14992b45df6e43b6025971e1e6fbf7
iamkongfused <117948607+iamkongfused@users.noreply.github.com>
2024-02-02 10:06:12 -0500

    Add files via upload
    Added Comments and fixed variables

--- square-and-multiply.py
+++ square-and-multiply.py
@@ -2,18 +2,42 @@
 
 
 
-n = 31313
-b = 4913
+
 
 x1 = 2388 #cat
 x2 = 1371 #dog
 
 def sq_n_mul(num):
-    binary_rep = bin(num)[2:]
+    n = 31313
+    b = 4913
+    """
+    Convert exponent 'b' to a binary string
+    minus the first 2 characters
+    """
+    binary_rep = bin(b)[2:]
+    
+    """
+    Start z at 1
+    """
     z = 1
+    
+    """
+    Do algorithm for as many characters in binary string
+    of exponent
+    """
     for i in len(binary_rep):
         if binary_rep[i] == '1':
+            """
+            if there is a 1 do the following equation:
+                z_(n+1) = z^2 * our number mod n
+            """
             z = (z**2 * num) % n
         else:
+            """
+            otherwise there must be a zero by rules of
+            binary strings and we do this formula:
+                z_(n+1) = z^2
+            """
             z = (z ** 2) % n
+    return n
             
