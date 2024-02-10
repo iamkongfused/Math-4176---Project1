a08e39ce14bda5d250844d985665e26cf6b73160
BuiHarrison <bharrison20@vt.edu>
2024-02-10 13:05:59 -0500

    updated table format to align with lecture notes example

--- square-and-multiply.py
+++ square-and-multiply.py
@@ -4,13 +4,15 @@ This program computes x^b mod n using the Square and Multiply Algorithm.
 x = input plaintext number
 
 Authors: Caleb Kong, Harrison Bui, Tide Adewunmi, Bobby Whitehead
-Date: February 2, 2024
+Date: February 10, 2024
 Course: MATH 4176
 """
+
 n = 31313
 b = 4913
 x1 = 2388  # cat
 x2 = 1371  # dog
+
 
 def sq_n_mul(x):
     """
@@ -31,8 +33,10 @@ def sq_n_mul(x):
     COL_LENGTH = 20
     print("i\t| b_i\t| %s| z" % ("".ljust(COL_LENGTH)))
     print("--------------------------------------------")
+    print("\t|    \t| %s| 1" % ("".ljust(COL_LENGTH)))
     for i in range(len(binary_rep)):
         prev_z = z
+        rowIndexDisplay = len(binary_rep) - 1 - i
         if binary_rep[i] == '1':
             """
             if there is a 1 do the following equation:
@@ -41,7 +45,7 @@ def sq_n_mul(x):
             z = (z ** 2 * x) % n
             # prints result in a row for this step
             computation = (str(prev_z) + "^2 * " + str(x)).ljust(COL_LENGTH)
-            print("%d\t| 1\t\t| %s| %d" % (i, computation, z))
+            print("%d\t| 1\t\t| %s| %d" % (rowIndexDisplay, computation, z))
         else:
             """
             otherwise there must be a zero by rules of
@@ -51,7 +55,7 @@ def sq_n_mul(x):
             z = (z ** 2) % n
             # prints result in a row for this step
             computation = (str(prev_z) + "^2").ljust(COL_LENGTH)
-            print("%d\t| 0\t\t| %s| %d" % (i, computation, z))
+            print("%d\t| 0\t\t| %s| %d" % (rowIndexDisplay, computation, z))
     return z
 
 
